04f8064676758a6355fdd648e791f0037784d2e9
vineesh <vineeshw1994@gmail.com>
2023-12-26 22:41:53 +0530

    sidebar icons design completed

--- instagram/src/components/Sidebar/Sidebar.jsx
+++ instagram/src/components/Sidebar/Sidebar.jsx
@@ -1,28 +1,84 @@
-import {Box, Flex,Link} from '@chakra-ui/react'
-import {Link as RouterLink} from 'react-router-dom'
-import { InstagramLogo, InstagramMobileLogo } from '../../assets/constants'
-
+import { Avatar, Box, Flex, Link } from '@chakra-ui/react'
+import { Link as RouterLink } from 'react-router-dom'
+import { CreatePostLogo, InstagramLogo, InstagramMobileLogo, NotificationsLogo, SearchLogo } from '../../assets/constants'
+import {AiFillHome} from 'react-icons/ai'
+import { Tooltip } from '@chakra-ui/react';
 const Sidebar = () => {
+
+  const sidebarItems = [{
+    icon: <AiFillHome />,
+    text: 'Home',
+    Link: '/'
+  },
+  {
+    icon: <SearchLogo />,
+    text: 'Search',
+  },
+  {
+    icon: <NotificationsLogo />,
+    text: 'Notifications'
+  },
+  {
+    icon: <CreatePostLogo />,
+    text: 'Create',
+  },
+  {
+    icon: <Avatar size={'sm'} name='Burak Orkmez' src='/profilepic.png' />,
+    text: 'profile',
+    link: '/assprogrammer'
+  }
+  ]
+
   return (
     <Box
-    height={'100vh'}
-    borderRight={'1px solid'}
-    borderColor={'whiteAlpha.300'}
-    py={8}
-    possition={'sticky'}
-    top={0}
-    left={0}
-    px={{base:2,md:4}}
+      height={'100vh'}
+      borderRight={'1px solid'}
+      borderColor={'whiteAlpha.300'}
+      py={8}
+      possition={'sticky'}
+      top={0}
+      left={0}
+      px={{ base: 2, md: 4 }}
     >
       <Flex direction={'column'} gap={10} w={'full'} height={'full'}>
-        <Link to={'/'} as={RouterLink} pl={2} display={{base:'none',md:'block'}} cursor={'pointer'}>
+        <Link to={'/'} as={RouterLink} pl={2} display={{ base: 'none', md: 'block' }} cursor={'pointer'}>
           <InstagramLogo />
         </Link>
-        <Link to={'/'} as={RouterLink} p={2} display={{base:'block',md:'none'}} cursor={'pointer'} borderRadius={6} _hover={{
-          bg:'whiteAlpha.300'
+        <Link to={'/'} as={RouterLink} p={2} display={{ base: 'block', md: 'none' }} cursor={'pointer'} borderRadius={6} _hover={{
+          bg: 'whiteAlpha.300'
         }} w={10}>
           <InstagramMobileLogo />
         </Link>
+        <Flex direction={'column'} gap={5} cursor={'pointer'}>
+          {sidebarItems.map((item, index) => (
+            <Tooltip
+              key={index}
+              hasArrow
+              label={item.text}
+              placement='right'
+              ml={1}
+              openDelay={500}
+              display={{ base: 'block', md: 'none' }}
+            >
+              <Link
+                display={'flex'}
+                to={item.link || null}
+                as={RouterLink}
+                alignItems={'center'}
+                gap={4}
+                _hover={{ bg: 'whiteAlpha.400' }}
+                borderRadius={6}
+                p={2}
+                w={10}
+              >
+               {item.icon}
+               <Box display={{base:'none',md:'block'}}>
+                {item.text}
+               </Box>
+              </Link>
+            </Tooltip>
+          ))}
+        </Flex>
       </Flex>
     </Box>
   )
