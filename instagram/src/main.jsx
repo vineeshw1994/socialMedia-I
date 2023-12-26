48c96810b1a2649983e8c4b396ed5021030540d0
vineesh <vineeshw1994@gmail.com>
2023-12-26 10:42:47 +0530

    dark mode color setup and install the react-icons and react-routerDom

--- instagram/src/main.jsx
+++ instagram/src/main.jsx
@@ -2,16 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { ChakraProvider } from '@chakra-ui/react'
-
 import { extendTheme } from '@chakra-ui/react'
 
+const styles = {
+  global:(props)=> ({
+    body:{
+      bg:mode('gray.100','#000')(props),
+      color:mode('gray.800','whiteAlpha.900')(props)
+    }
+  })
+}
+
 const config = {
-  initialColorMode: 'light',
+  initialColorMode: 'dark',
   useSystemColorMode: false,
 }
 
 // 3. extend the theme
-const theme = extendTheme({ config })
+const theme = extendTheme({ config,styles })
 
 export default theme
 
