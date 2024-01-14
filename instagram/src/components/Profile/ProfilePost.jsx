f4d87e298961c32ce503c8c222f32ad1c78acec7
vineesh <vineeshw1994@gmail.com>
2024-01-14 23:36:21 +0530

    showing the image s in the profile

--- instagram/src/components/Profile/ProfilePost.jsx
+++ instagram/src/components/Profile/ProfilePost.jsx
@@ -1,7 +1,20 @@
+import { Flex, GridItem } from "@chakra-ui/react"
 
 const ProfilePost = () => {
   return (
-    <div>ProfilePost</div>
+    <GridItem cursor={'pointer'}
+    borderRadius={4}
+    overflow={'hidden'}
+    border={'1px solid'}
+    borderColor={'whiteAlpha.300'}
+    position={'relative'}
+    aspectRatio={1/1}
+    >
+    <Flex>
+
+    </Flex>
+
+    </GridItem>
   )
 }
 
